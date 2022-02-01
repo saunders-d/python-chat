390011394aeef1aece2955bcbd794e104dfe0761
Dakota Saunders <dakotabsaunders@gmail.com>
2022-02-01 18:46:37 -0500

    Create stubs for tentative methods

---
+++ chat.py
@@ -0,0 +1,32 @@
+import boto3
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Websocket:
+    def __init__(self):
+        pass
+
+    def create_api(self):
+        return
+
+    def add_perms(self):
+        return
+
+    def remove_perms(self):
+        return
+
+    def deploy_api(self):
+        return
+
+    def delete_api(self):
+        return
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
